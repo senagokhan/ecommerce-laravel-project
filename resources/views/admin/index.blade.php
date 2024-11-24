47fae81184ff6ff3db0575137a6519988111dbf1
Sena Gökhan <160348164+senagokhan@users.noreply.github.com>
2024-11-24 17:03:07 +0100

    authentication
    # Conflicts:
    #	README.md

---
+++ resources/views/admin/index.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name ="viewport" content="width=device-width,initiak-scale=1">
+    <title>
+
+    </title>
+ </head>
+<body>
+
+     <h1>ADMIN</h1>
+
+</body>
+</html>
