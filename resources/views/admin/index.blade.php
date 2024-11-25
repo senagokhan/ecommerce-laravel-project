f2831a72ccf87281a04d6657a83a8db487af6a36
Sena Gökhan <160348164+senagokhan@users.noreply.github.com>
2024-11-25 19:34:39 +0100

    admin-template

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -1,22 +1,30 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <meta charset="UTF-8">
-    <meta name ="viewport" content="width=device-width,initiak-scale=1">
-    <title>
-
-    </title>
- </head>
+    @include('admin.css')
+</head>
 <body>
+    @include('admin.header')
 
-     <h1>ADMIN</h1>
+    @include('admin.sidebar')
+    <!-- Sidebar Navigation end-->
+    <div class="page-content">
+        <div class="page-header">
+            <div class="container-fluid">
 
+                @include('admin.body')
 
-     <form method="POST" action="{{ route('logout') }}">
-         @csrf
-         <input type = "submit" value="Logout">
-
-     </form>
-
+       </div>
+    </div>
+</div>
+<!-- JavaScript files-->
+<script src="{{asset('admincss/vendor/jquery/jquery.min.js')}}"></script>
+<script src="{{asset('admincss/vendor/popper.js/umd/popper.min.js')}}"> </script>
+<script src="{{asset('admincss/vendor/bootstrap/js/bootstrap.min.js')}}"></script>
+<script src="{{asset('admincss/vendor/jquery.cookie/jquery.cookie.js')}}"> </script>
+<script src="{{asset('admincss/vendor/chart.js/Chart.min.js')}}"></script>
+<script src="{{asset('admincss/vendor/jquery-validation/jquery.validate.min.js')}}"></script>
+<script src="{{asset('admincss/js/charts-home.js')}}"></script>
+<script src="{{asset('admincss/js/front.js')}}"></script>
 </body>
 </html>
