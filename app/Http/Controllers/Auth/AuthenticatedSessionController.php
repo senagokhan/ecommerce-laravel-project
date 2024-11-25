0f7ef1361ab6e31f54ff2f80c353d4a55145f212
Sena Gökhan <160348164+senagokhan@users.noreply.github.com>
2024-11-25 13:22:57 +0100

    Authentication

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -29,6 +29,7 @@ class AuthenticatedSessionController extends Controller
         $request->session()->regenerate();
 
         if ($request->user()->usertype == 'admin') {
+
             return redirect('admin/dashboard');
         }
 
--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -11,5 +11,12 @@
 
      <h1>ADMIN</h1>
 
+
+     <form method="POST" action="{{ route('logout') }}">
+         @csrf
+         <input type = "submit" value="Logout">
+
+     </form>
+
 </body>
 </html>
